5208a4c8eec3ae80e67a84cd9f44bc532b9078b5
Gustavo Coutinho De Faria <gustavocfaria@MacBook-Air-6.local>
2026-05-14 22:39:54 -0300

    PWA completo: manifest, service worker, ícones

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'brew-temp-v1';
+const ASSETS = [
+  '/coffee-temp/',
+  '/coffee-temp/index.html',
+  '/coffee-temp/manifest.json',
+  '/coffee-temp/icon-192.png',
+  '/coffee-temp/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
